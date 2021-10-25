f64062c68b7db3732d0587d3b63b32903f24f871
Leonardo Simoza <simozaleonardo@gmail.com>
2021-10-25 07:32:51 -0500

    First commit

---
+++ Podfile
@@ -0,0 +1,26 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '14.0'
+
+# Base Pods
+def base_pods
+  pod 'Alamofire', '~> 5.2'
+end
+
+
+target 'WBooks' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WBooks
+  base_pods
+
+  target 'WBooksTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WBooksUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ WBooksUITests/WBooksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WBooksUITestsLaunchTests.swift
+//  WBooksUITests
+//
+//  Created by leonardo.a.simoza on 22/10/21.
+//
+
+import XCTest
+
+class WBooksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
