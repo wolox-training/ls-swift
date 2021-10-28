3f81500d085d4c1c9eddd5b00d7ea7697b43556e
Leonardo Simoza <simozaleonardo@gmail.com>
2021-10-28 10:59:53 -0500

    Removed spaces

--- WBooks/Common/Util/Extensions/String+Extension.swift
+++ WBooks/Common/Util/Extensions/String+Extension.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 extension String {
-    
     func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
         return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
     }
--- WBooks/Common/Util/Extensions/UIColor+Extension.swift
+++ WBooks/Common/Util/Extensions/UIColor+Extension.swift
@@ -14,9 +14,7 @@ enum AssetsColor: String {
 }
 
 extension UIColor {
-    
     static func appColor(_ name: AssetsColor) -> UIColor? {
         return UIColor(named: name.rawValue);
     }
-    
 }
